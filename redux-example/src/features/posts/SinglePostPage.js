8c35de0dd0007ebf86491433d660b6fdf9c1e14d
Cleomir <cleomir.mg@hotmail.com>
2021-03-04 17:55:55 +0200

    Add edit posts feature

--- redux-example/src/features/posts/SinglePostPage.js
+++ redux-example/src/features/posts/SinglePostPage.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const SinglePostPage = ({ match }) => {
   const { postId } = match.params;
@@ -18,6 +19,9 @@ const SinglePostPage = ({ match }) => {
       <article className="post">
         <h2>{post.article}</h2>
         <p className="post-content">{post.content}</p>
+        <Link to={`/editPost/${post.id}`} className="button">
+          Edit Post
+        </Link>
       </article>
     </section>
   );
