7f0a9f41c94685afd6a987f883627bfa27e3f1e2
Cleomir <cleomir.mg@hotmail.com>
2021-03-07 21:07:19 +0200

    Add thunk reducers

--- redux-example/src/features/posts/PostsList.js
+++ redux-example/src/features/posts/PostsList.js
@@ -1,34 +1,39 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import PostAuthor from "./PostAuthor";
-import ReactionButtons from "./ReactionButtons";
-import TimeAgo from "./TimeAgo";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { selectAllPosts, fetchPosts } from "./postsSlice";
+import PostExcerpt from "./PostExcerpt";
 
 const PostsList = () => {
-  const posts = useSelector((state) => state.posts);
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
-  const renderedPosts = orderedPosts.map((post) => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <p className="post-content">
-        {post.content.substring(0, 100)}
-        <br />
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-      <Link to={`/posts/${post.id}`} className="button muted-button">
-        View Post
-      </Link>
-    </article>
-  ));
+  const dispatch = useDispatch();
+  const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector((state) => state.posts.status);
+  const error = useSelector((state) => state.posts.error);
+  let content;
+
+  if (postStatus === "loading") {
+    content = <div className="loader">Loading...</div>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <div>{error}</div>;
+  }
+
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
 
   return (
     <section className="posts-list">
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
